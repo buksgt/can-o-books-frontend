4a507b420f702aaa080b90324702e0f8c4b73fd3
Ben Borden <benjaminborden1@gmail.com>
2024-04-10 07:03:43 -0700

    updating bestbooks and app.jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,89 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import BestBooks from './BestBooks';
-import About from './About'; // Import the About component
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Router>
-          <Header />
-          <nav>
-            <Link to="/">Home</Link> | <Link to="/about">About</Link>
-          </nav>
-          <Routes>
-            <Route exact path="/" element={<BestBooks />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-          <Footer />
-        </Router>
-      </>
-    )
+import Books from './BestBooks.jsx';
+//  when export, make sure you call it "BestBooks"
+
+function App() {
+
+  const [books, setBooks] = useState([]);
+  
+  useEffect( () => {
+     getBooks();
+  }, []);
+
+  async function getBooks() {
+    try {
+      const response = await axios.get('https://can-of-books-backend-qs90.onrender.com/books');
+      setBooks(response.data);
+    } catch (error) {
+      console.error(error);
+    }
   }
-}
+
+  async fucntion deleteBook(id) {
+
+  try {
+    const response = await axios.delete(`https://can-of-books-backend-qs90.onrender.com/books/${id}`);
+    let deleteBooks = response.data;
+
+    let newBooks = books.filter( function(book) {
+      return book._id !== deleteBooks._id;
+    });
+    setBooks(newBooks);
+
+  } catch (error) {
+    console.error(error);
+
+    }
+  }
+
+  async function addBook(book) {
+    try {
+      let response = await axios.post('https://can-of-books-backend-qs90.onrender.com/books', book);
+      let newBook = response.data;
+      setBooks([...books, newBook]);
+   } catch(error) {
+    console.error(error);
+   }
+  }
+
+  return (
+    <>
+    <Books handleAddBook={addBook} handleDelete={deleteBook} books={books} />
+    </>
+  )
+} 
+
 
 export default App;
+
+// import React from 'react';
+// import Header from './Header';
+// import Footer from './Footer';
+// import BestBooks from './BestBooks';
+// import About from './About'; // Import the About component
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <Router>
+//           <Header />
+//           <nav>
+//             <Link to="/">Home</Link> | <Link to="/about">About</Link>
+//           </nav>
+//           <Routes>
+//             <Route exact path="/" element={<BestBooks />} />
+//             <Route path="/about" element={<About />} />
+//           </Routes>
+//           <Footer />
+//         </Router>
+//       </>
+//     )
+//   }
+// }
+
+// export default App;
--- src/BestBooks.jsx
+++ src/BestBooks.jsx
@@ -1,58 +1,114 @@
-import React from 'react';
-import axios from 'axios';
-import { Carousel } from 'react-bootstrap';
+import { useState } from 'react'
 
-class BestBooks extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: []
-    }
+function Books(props) {
+
+  const [bookData, setBookData] = useState({});
+
+  // async function deleteTheBook(event) {
+  //   let id = event.target.id;
+  //   props.handleDelete(id);
+  // }
+
+  function handleChange(event) {
+    let value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    setBookData({
+      ...bookData, [event.target.name]: value
+    });
   }
 
-  componentDidMount() {
-    axios.get('/books')
-      .then(response => {
-        this.setState({ books: response.data });
-      })
-      .catch(error => {
-        console.error('Error fetching books', error);
-      });
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(bookData)
+    props.handleAddBook(bookData);
   }
 
-  renderBooks() {
-    return this.state.books.map(book => (
-      <Carousel.Item key={book.id}>
-        <img
-          className="d-block w-100"
-          src={book.coverImageUrl}
-          alt={book.title}
-        />
-        <Carousel.Caption>
-          <h3>{book.title}</h3>
-          <p>{book.description}</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    ));
-  }
+  return (
+    <>
+      <h2>Books</h2>
+      <form onSubmit={handleSUbmit}>
+        <div><input name="title" type="text" placeholder="Title" onChange={handleChange} /></div>
+        <div><input name="author" type="text" placeholder="Author" onChange={handleChange} /></div>
+        <div><input name="available" type="checkbox" onChange={handleChange} /><span>Available</span></div>
+      </form>
+      <section>
+        {props.books.map(book =>
+          <div key={book_id}>
+            <h2>{book.title}</h2>
+            <p>Title: {book.title}</p>
+            <p>Author: {book.author}</p>
+            <p>Is Available: {book.isAvailabe.toString()}</p>
+          </div>
+        )
+        }
+      </section>
+    </>
+  );
 
-  render() {
-    const { books } = this.state;
 
-    return (
-      <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {books.length ? (
-          <Carousel>
-            {this.renderBooks()}
-          </Carousel>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
-      </>
-    );
-  }
+
 }
 
 export default BestBooks;
+
+
+
+
+// import React from 'react';
+// import axios from 'axios';
+// import { Carousel } from 'react-bootstrap';
+
+// class BestBooks extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       books: []
+//     }
+//   }
+
+//   componentDidMount() {
+//     axios.get('https://can-of-books-backend-qs90.onrender.com/books')
+//       .then(response => {
+//         this.setState({ books: response.data });
+//       })
+//       .catch(error => {
+//         console.error('Error fetching books', error);
+//       });
+//   }
+
+//   renderBooks() {
+//     return this.state.books.map(book => (
+//       <Carousel.Item key={book.id}>
+//         <img
+//           className="d-block w-100"
+//           src={book.coverImageUrl}
+//           alt={book.title}
+//         />
+//         <Carousel.Caption>
+//           <h3>{book.title}</h3>
+//           <p>{book.description}</p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//     ));
+//   }
+
+//   render() {
+//     const { books } = this.state;
+
+//     return (
+//       <>
+//         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+
+//         {books.length ? (
+//           <Carousel>
+//             {this.renderBooks()}
+//           </Carousel>
+//         ) : (
+//           <h3>No Books Found :(</h3>
+//         )}
+//       </>
+//     );
+//   }
+// }
+
+// export default BestBooks;
